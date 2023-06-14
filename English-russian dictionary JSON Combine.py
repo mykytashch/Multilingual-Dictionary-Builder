742c196ed8a4603ea0d5a038868b0e97c10b1f05
Mykyta Shcheholevatyi <129088502+mykytashch@users.noreply.github.com>
2023-06-14 23:52:05 +0200

    Add files via upload

--- English-russian dictionary JSON Combine.py
+++ English-russian dictionary JSON Combine.py
@@ -4,13 +4,114 @@ from tkinter import ttk, filedialog
 import json
 import nltk
 from nltk.corpus import wordnet as wn
-import requests
-from bs4 import BeautifulSoup
 import random
 import time
 
 # Загрузка WordNet
 nltk.download('wordnet')
+
+# Список сайтов и соответствующих им конфигураций
+websites = {
+    "Site 1": {
+        "url": None,
+        "config_file": None,
+        "parse_function": None
+    },
+    "Site 2": {
+        "url": None,
+        "config_file": None,
+        "parse_function": None
+    },
+    "Site 3": {
+        "url": None,
+        "config_file": None,
+        "parse_function": None
+    }
+}
+
+# Загрузка конфигурации для каждого сайта
+site_configs = {}
+
+
+def load_config_file(site):
+    file_path = filedialog.askopenfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+    if file_path:
+        with open(file_path, "r") as file:
+            site_configs[site] = json.load(file)
+
+
+def open_config_file(site):
+    load_config_file(site)
+
+
+def create_site_fields():
+    row = 150
+    for site in websites:
+        label = tk.Label(frame, text=site, font=("Helvetica", 12), foreground="white")
+        label.place(x=50, y=row)
+
+        url_entry = tk.Entry(frame, font=("Helvetica", 12))
+        url_entry.place(x=200, y=row)
+
+        config_entry = tk.Entry(frame, font=("Helvetica", 12))
+        config_entry.place(x=400, y=row, width=250)
+
+        browse_button = ttk.Button(frame, text="Обзор", command=lambda s=site: open_config_file(s))
+        browse_button.place(x=660, y=row)
+
+        parse_checkbutton = ttk.Checkbutton(frame, text="Парсинг", variable=websites[site]["parse_function"])
+        parse_checkbutton.place(x=740, y=row)
+
+        websites[site]["url"] = url_entry
+        websites[site]["config_file_entry"] = config_entry
+
+        row += 30
+
+
+def create_json():
+    start_line = int(start_entry.get())
+    end_line = int(end_entry.get())
+
+    with open("236k-of-words.txt", "r") as file:
+        lines = file.readlines()[start_line - 1:end_line]
+        words = [line.strip() for line in lines]
+
+    data = []
+
+    for word in words:
+        word_data = {"word": word}
+
+        for site, config in site_configs.items():
+            url = websites[site]["url"].get()
+            parse_function = websites[site]["parse_function"].get()
+            if parse_function:
+                parsed_data = parse_function(url, config)
+                word_data.update(parsed_data)
+
+            # Задержка перед каждым запросом
+            if use_delay.get():
+                delay = random.uniform(0.5, 2.0)
+                time.sleep(delay)
+
+        data.append(word_data)
+
+    json_data = json.dumps(data, indent=4)
+    output_text.delete("1.0", tk.END)
+    output_text.insert(tk.END, json_data)
+
+
+def export_json():
+    json_data = output_text.get("1.0", tk.END)
+    file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+    if file_path:
+        with open(file_path, "w") as file:
+            file.write(json_data)
+
+
+def select_all(event):
+    output_text.tag_add("sel", "1.0", "end")
+    return "break"
+
 
 def parse_site1(url, config):
     # Реализация парсинга для Site 1
@@ -53,6 +154,32 @@ def parse_site3(url, config):
     }
 
 
+def toggle_parse_function(site):
+    parse_function = websites[site]["parse_function"]
+    if parse_function.get():
+        parse_function.set(None)
+    else:
+        parse_function.set(parse_site1)
+
+
+def toggle_proxy():
+    if use_proxy.get():
+        # Включить использование прокси
+        pass
+    else:
+        # Отключить использование прокси
+        pass
+
+
+def toggle_delay():
+    if use_delay.get():
+        # Включить использование задержки
+        pass
+    else:
+        # Отключить использование задержки
+        pass
+
+
 # Создание главного окна
 window = tk.Tk()
 window.title("English-russian dictionary JSON Combine")
@@ -81,113 +208,37 @@ end_label.place(x=50, y=100)
 end_entry = tk.Entry(frame, font=("Helvetica", 12))
 end_entry.place(x=400, y=100)
 
-# Список сайтов и соответствующих им конфигураций
-websites = {
-    "Site 1": {
-        "url": None,
-        "config_file": None,
-        "parse_function": parse_site1
-    },
-    "Site 2": {
-        "url": None,
-        "config_file": None,
-        "parse_function": parse_site2
-    },
-    "Site 3": {
-        "url": None,
-        "config_file": None,
-        "parse_function": parse_site3
-    }
-}
-
-# Загрузка конфигурации для каждого сайта
-site_configs = {}
-
-def create_json():
-    start_line = int(start_entry.get())
-    end_line = int(end_entry.get())
-
-    with open("236k-of-words.txt", "r") as file:
-        lines = file.readlines()[start_line - 1:end_line]
-        words = [line.strip() for line in lines]
-
-    data = []
-
-    for word in words:
-        word_data = {"word": word}
-
-        for site, config in site_configs.items():
-            url = websites[site]["url"].get()
-            parse_function = websites[site]["parse_function"]
-            parsed_data = parse_function(url, config)
-
-            word_data.update(parsed_data)
-
-            # Задержка перед каждым запросом
-            delay = random.uniform(0.5, 2.0)
-            time.sleep(delay)
-
-        data.append(word_data)
-
-    json_data = json.dumps(data, indent=4)
-    output_text.delete("1.0", tk.END)
-    output_text.insert(tk.END, json_data)
-
-
-def export_json():
-    json_data = output_text.get("1.0", tk.END)
-    file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
-    if file_path:
-        with open(file_path, "w") as file:
-            file.write(json_data)
-
-
-def select_all(event):
-    output_text.tag_add("sel", "1.0", "end")
-    return "break"
-
-
-def open_config_file(site):
-    file_path = filedialog.askopenfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
-    if file_path:
-        websites[site]["config_file"] = file_path
-        with open(file_path, "r") as file:
-            site_configs[site] = json.load(file)
-
-
 # Создание полей и кнопок для настроек каждого сайта
-row = 150
-for site, config in websites.items():
-    label = tk.Label(frame, text=site, font=("Helvetica", 12), foreground="white")
-    label.place(x=50, y=row)
-
-    url_entry = tk.Entry(frame, font=("Helvetica", 12))
-    url_entry.place(x=200, y=row)
-
-    config_entry = tk.Entry(frame, font=("Helvetica", 12))
-    config_entry.place(x=400, y=row, width=250)
-
-    browse_button = ttk.Button(frame, text="Обзор", command=lambda s=site: open_config_file(s))
-    browse_button.place(x=660, y=row)
-
-    websites[site]["url"] = url_entry
-    websites[site]["config_file_entry"] = config_entry
-
-    row += 30
+create_site_fields()
 
 # Настройка кнопки для создания JSON
 create_button = ttk.Button(frame, text="Создать JSON'ы", command=create_json)
-create_button.place(x=200, y=row)
+create_button.place(x=200, y=450)
 
 # Настройка кнопки для экспорта JSON
 export_button = ttk.Button(frame, text="Экспорт JSON", command=export_json)
-export_button.place(x=350, y=row)
+export_button.place(x=350, y=450)
 
 # Настройка текстового поля для вывода JSON
 output_text = tk.Text(frame, font=("Helvetica", 12), height=10)
-output_text.place(x=50, y=row + 50, width=600)
+output_text.place(x=50, y=500, width=700)
 output_text.bind("<Command-a>", select_all)
 
+# Настройка чекбоксов для включения/отключения парсинга, использования прокси и задержки
+row = 150
+for site in websites:
+    parse_checkbutton = ttk.Checkbutton(frame, text="Парсинг", variable=websites[site]["parse_function"])
+    parse_checkbutton.place(x=740, y=row)
+    row += 30
+
+use_proxy = tk.BooleanVar()
+proxy_checkbutton = ttk.Checkbutton(frame, text="Использовать прокси", variable=use_proxy)
+proxy_checkbutton.place(x=700, y=50)
+
+use_delay = tk.BooleanVar()
+delay_checkbutton = ttk.Checkbutton(frame, text="Использовать задержку", variable=use_delay)
+delay_checkbutton.place(x=700, y=100)
+
 # Запуск главного цикла приложения
-window.geometry("800x550")
+window.geometry("800x600")
 window.mainloop()
